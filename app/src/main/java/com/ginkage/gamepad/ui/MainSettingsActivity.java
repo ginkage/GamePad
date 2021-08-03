5ab811819ec8c614c0eff284619cf3a3597024d3
Ivan Podogov <ginkage@google.com>
2021-08-03 16:29:23 +0100

    Simplify permissions request

--- app/src/main/java/com/ginkage/gamepad/ui/MainSettingsActivity.java
+++ app/src/main/java/com/ginkage/gamepad/ui/MainSettingsActivity.java
@@ -16,57 +16,40 @@
 
 package com.ginkage.gamepad.ui;
 
-import android.Manifest;
 import android.Manifest.permission;
-import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Build.VERSION;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
-import java.util.ArrayList;
-import java.util.List;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts.RequestMultiplePermissions;
+import java.util.Map;
 
 /** Main settings activity menu, mostly static. */
 public class MainSettingsActivity extends CommonPreferenceActivity {
 
-    @Override
-    public void onRequestPermissionsResult(
-        int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 1) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+    ActivityResultLauncher<String[]> launcher =
+        registerForActivityResult(new RequestMultiplePermissions(),
+            result -> {
+                for (Map.Entry<String, Boolean> entry : result.entrySet()) {
+                    if (!entry.getValue()) {
+                        finish();
+                    }
+                }
                 startPreferenceFragment(new PairedDevicesFragment(), false);
-                return;
-            }
-            finish();
-        }
-    }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         if (VERSION.SDK_INT >= VERSION_CODES.S) {
-            List<String> missingPermissions = new ArrayList<>();
-            if (checkSelfPermission(permission.BLUETOOTH_SCAN)
-                != PackageManager.PERMISSION_GRANTED) {
-                missingPermissions.add(permission.BLUETOOTH_SCAN);
-            }
-            if (checkSelfPermission(permission.BLUETOOTH_ADVERTISE)
-                != PackageManager.PERMISSION_GRANTED) {
-                missingPermissions.add(permission.BLUETOOTH_ADVERTISE);
-            }
-            if (checkSelfPermission(permission.BLUETOOTH_CONNECT)
-                != PackageManager.PERMISSION_GRANTED) {
-                missingPermissions.add(permission.BLUETOOTH_CONNECT);
-            }
-
-            if (!missingPermissions.isEmpty()) {
-                requestPermissions(missingPermissions.toArray(new String[0]), 1);
-                return;
-            }
+            launcher.launch(new String[] {
+                permission.BLUETOOTH_SCAN,
+                permission.BLUETOOTH_ADVERTISE,
+                permission.BLUETOOTH_CONNECT
+            });
+        } else {
+            startPreferenceFragment(new PairedDevicesFragment(), false);
         }
-
-        startPreferenceFragment(new PairedDevicesFragment(), false);
     }
 }
